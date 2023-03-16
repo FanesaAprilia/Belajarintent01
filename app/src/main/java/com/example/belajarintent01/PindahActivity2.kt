26c93eb5391afa261303e953829d90430f071a60
FanesaAprilia <2110031802116@sar.ac.id>
2023-03-16 11:03:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/belajarintent01/PindahActivity2.kt
@@ -0,0 +1,11 @@
+package com.example.belajarintent01
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PindahActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pindah2)
+    }
+}
